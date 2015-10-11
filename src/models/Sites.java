5f7f9b9bb015ffb9f14a9711e01d2690b326effb
Isaac <Isaac2709@hotmail.com>
2015-10-11 17:48:17 -0600

    Add sites class

---
+++ src/models/Sites.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author isaac
+ */
+public class Sites {
+    private String title;
+    private String languaje;
+    private ArrayList<String> listTags;
+    private String header;
+    private String body;
+    private String footer;
+}
