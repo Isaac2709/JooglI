c3c0b3b93e9647945a161e3761d74b9cd9cade55
Isaac <Isaac2709@hotmail.com>
2015-10-11 20:10:30 -0600

    Set the basic attributes in the SearchEngine model

---
+++ src/models/SearchEngine.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author isaac
+ */
+public class SearchEngine {
+    private String search;
+    
+    public SearchEngine(){
+        
+    }
+
+    public SearchEngine(String search) {
+        this.search = search;
+    }
+            
+    public ArrayList<Sites> search(){
+        return null;
+    }
+}
