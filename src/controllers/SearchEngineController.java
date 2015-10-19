d793590f08aa016a45141e1018d0b4880eb0c66b
Isaac <Isaac2709@hotmail.com>
2015-10-18 21:31:49 -0600

    Update the search method to analyze 'manually' the web site (word by word)

--- src/controllers/SearchEngineController.java
+++ src/controllers/SearchEngineController.java
@@ -5,17 +5,21 @@
  */
 package controllers;
 
+import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import models.SearchEngine;
 import models.Sites;
+import models.Token;
 import org.jsoup.Jsoup;
 
 /**
@@ -32,38 +36,83 @@ public class SearchEngineController {
     
     public ArrayList<Sites> search(String strSearch){
         ArrayList<Sites> listMatchingTittles = new ArrayList<>();
-        ArrayList<Sites> listMatchingBody = new ArrayList<>();
+        ArrayList<Sites> listMatchingBody = new ArrayList<>();        
         for(int i = 0; i < listSites.size(); i++){
-            Pattern regPatt = Pattern.compile(strSearch.toLowerCase());
-            Matcher regMatch = regPatt.matcher(listSites.get(i).getTitle().toLowerCase());            
+            boolean matchingTitle = false;
+            StringTokenizer tokens = new StringTokenizer(strSearch);
+            ArrayList<Token> listTokensMatches = new ArrayList<Token>();
             int intMatch = 0;
-            while(regMatch.find())
-            {
-                intMatch++;                
-            }
-            if(intMatch > 0){
-                //String match = regMatch.group();
-                System.out.println("Match "+ strSearch + " in title: " + listSites.get(i).getTitle());
-                listMatchingTittles.add(listSites.get(i));
-            }
-            else{
-                regMatch = regPatt.matcher(listSites.get(i).getBody());            
-                intMatch = 0;
-                while(regMatch.find())
-                {
-                    //String match = regMatch.group();
-                    //System.out.println("Match "+ strSearch + " in body: " + match);
-                    intMatch++;                
+            Token currentToken = null;
+            while(tokens.hasMoreTokens()){                
+                int numberMatches = 0;
+                String token = tokens.nextToken();
+                // Compare the tokens with the title of the web-site
+                String textTitle = listSites.get(i).getTitle().toLowerCase();
+                for(int n = 0; n< textTitle.length(); n++){
+                    if(textTitle.charAt(n) == token.charAt(0)){
+                        int tempN = n; boolean match = true;
+                        for(int m = 0; m < token.length(); m++){
+                            if(textTitle.charAt(tempN) != token.charAt(m)){
+                                match = false;
+                                break;
+                            }
+                            tempN++;
+                        }
+                        if(match){
+                            numberMatches++;
+                        }
+                    }
                 }
-                if(intMatch > 0){                                
+                if(numberMatches > 0){ // If found any match in the tittle, this will be prioritized
+                    matchingTitle = true;
+                }
+                
+                // Compare the tokens with the body of the web-site
+                String textBody = listSites.get(i).getBody().toLowerCase();
+                for(int n = 0; n < textBody.length(); n++){
+                    if(textBody.charAt(n) == token.charAt(0)){
+                        int tempN = n; boolean match = true;
+                        for(int m = 0; m < token.length(); m++){
+                            if(textBody.charAt(tempN) != token.charAt(m)){
+                                match = false;
+                                break;
+                            }
+                        }
+                        if(match){
+                            numberMatches++;
+                        }
+                    }
+                }
+                // If exist more than zero matches of token, it added to list tokens match list
+                if(numberMatches > 0){
+                    listTokensMatches.add(new Token(token, numberMatches));
+                }
+            }
+            if(!listTokensMatches.isEmpty()){
+                listSites.get(i).setListTokensMatches(listTokensMatches);
+                if(matchingTitle){
+                    listMatchingTittles.add(listSites.get(i));
+                }
+                else{
                     listMatchingBody.add(listSites.get(i));
-                }   
-            }            
+                }
+            }                
         }
         ArrayList<Sites> listMatching = new ArrayList<>();
         listMatching.addAll(listMatchingTittles);
         listMatching.addAll(listMatchingBody);        
         return listMatching;
+    }
+    
+    public void openWebSite(String webAdress){
+        if(java.awt.Desktop.isDesktopSupported()){
+            try{
+                 Desktop dk = Desktop.getDesktop();
+                 dk.browse(new URI(webAdress));
+            }catch(Exception e){
+                System.out.println("Error al abrir URL: "+e.getMessage());
+            }
+        }  
     }
     
     public ArrayList<Sites> consultSites(){
