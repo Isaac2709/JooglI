7142e496e5aa4a51750ecf02b947c295eaa70718
Isaac <Isaac2709@hotmail.com>
2015-10-24 20:08:40 -0600

    Update the search method to allow use threads

--- src/controllers/SearchEngineController.java
+++ src/controllers/SearchEngineController.java
@@ -20,6 +20,7 @@ import java.util.regex.Pattern;
 import models.SearchEngine;
 import models.Sites;
 import models.Token;
+import multicore.ParallelTasks;
 import org.jsoup.Jsoup;
 
 /**
@@ -29,81 +30,154 @@ import org.jsoup.Jsoup;
 public class SearchEngineController {
     private ArrayList<Sites> listSites;
     private static Connection conn;
+    private static int index;
     
     public SearchEngineController(){
         conn = new SearchEngine().getConn();
+        index = 0;
     }
     
-    public ArrayList<Sites> search(String strSearch){
+    public ArrayList<Sites> search(String strSearch, boolean implementsMultiCore) throws InterruptedException{
         ArrayList<Sites> listMatchingTittles = new ArrayList<>();
-        ArrayList<Sites> listMatchingBody = new ArrayList<>();        
-        for(int i = 0; i < listSites.size(); i++){
-            boolean matchingTitle = false;
-            StringTokenizer tokens = new StringTokenizer(strSearch);
-            ArrayList<Token> listTokensMatches = new ArrayList<Token>();
-            int intMatch = 0;
-            Token currentToken = null;
-            while(tokens.hasMoreTokens()){                
-                int numberMatches = 0;
-                String token = tokens.nextToken();
-                // Compare the tokens with the title of the web-site
-                String textTitle = listSites.get(i).getTitle().toLowerCase();
-                for(int n = 0; n< textTitle.length(); n++){
-                    if(textTitle.charAt(n) == token.charAt(0)){
-                        int tempN = n; boolean match = true;
-                        for(int m = 0; m < token.length(); m++){
-                            if(textTitle.charAt(tempN) != token.charAt(m)){
-                                match = false;
-                                break;
+        ArrayList<Sites> listMatchingBody = new ArrayList<>();                
+        
+        if(implementsMultiCore){                
+            index = 0;
+            ParallelTasks tasks = new ParallelTasks();
+            final Runnable waitOneSecond = new Runnable() {
+                public void run()
+                {
+                    try
+                    {                        
+                        boolean matchingTitle = searchByWebSite(listSites.get(index), strSearch);
+                        if((listSites.get(index).getListTokensMatches()!=null) && (!listSites.get(index).getListTokensMatches().isEmpty())){
+                            if(matchingTitle){
+                                listMatchingTittles.add(listSites.get(index));
                             }
-                            tempN++;
+                            else{
+                                listMatchingBody.add(listSites.get(index));                
+                            }
                         }
-                        if(match){
-                            numberMatches++;
-                        }
+                        matchingTitle = false;
+                        //Thread.sleep(1000);
+
+                    }
+                    catch (Exception e)//Interrupted
+                    {
                     }
                 }
-                if(numberMatches > 0){ // If found any match in the tittle, this will be prioritized
-                    matchingTitle = true;
-                }
-                
-                // Compare the tokens with the body of the web-site
-                String textBody = listSites.get(i).getBody().toLowerCase();
-                for(int n = 0; n < textBody.length(); n++){
-                    if(textBody.charAt(n) == token.charAt(0)){
-                        int tempN = n; boolean match = true;
-                        for(int m = 0; m < token.length(); m++){
-                            if(textBody.charAt(tempN) != token.charAt(m)){
-                                match = false;
-                                break;
+            };
+            for(int i = 0; i < listSites.size(); i++){
+                //index = i;                
+                System.out.println("1Index " + index);
+                tasks.add(new Runnable() {
+                    public void run()
+                    {
+                        try
+                        {                             
+                            boolean matchingTitle = searchByWebSite(listSites.get(index), strSearch);
+                            if((listSites.get(index).getListTokensMatches()!=null) && (!listSites.get(index).getListTokensMatches().isEmpty())){
+                                if(matchingTitle){
+                                    listMatchingTittles.add(listSites.get(index));
+                                }
+                                else{
+                                    listMatchingBody.add(listSites.get(index));                
+                                }
                             }
-                            tempN++;
+                            matchingTitle = false;
+                            index++;
+                            //Thread.sleep(10);
+
                         }
-                        if(match){
-                            numberMatches++;
+                        catch (Exception e)//Interrupted
+                        {
                         }
                     }
-                }
-                // If exist more than zero matches of token, it added to list tokens match list
-                if(numberMatches > 0){
-                    listTokensMatches.add(new Token(token, numberMatches));
-                }
+                });
             }
-            if(!listTokensMatches.isEmpty()){
-                listSites.get(i).setListTokensMatches(listTokensMatches);
-                if(matchingTitle){
-                    listMatchingTittles.add(listSites.get(i));
+            final long start = System.currentTimeMillis();
+            tasks.go();
+            System.err.println(System.currentTimeMillis() - start);
+        }
+        else{        
+            final long start = System.currentTimeMillis();
+            for(int i = 0; i < listSites.size(); i++){
+                boolean matchingTitle = searchByWebSite(listSites.get(i), strSearch);
+                if((listSites.get(i).getListTokensMatches()!=null) && (!listSites.get(i).getListTokensMatches().isEmpty())){
+                    if(matchingTitle){
+                        listMatchingTittles.add(listSites.get(i));
+                    }
+                    else{
+                        listMatchingBody.add(listSites.get(i));                
+                    }
                 }
-                else{
-                    listMatchingBody.add(listSites.get(i));
-                }
+                matchingTitle = false;
             }
-            matchingTitle = false;
+            System.err.println(System.currentTimeMillis() - start);
         }
         ArrayList<Sites> listMatching = new ArrayList<>();
         listMatching.addAll(listMatchingTittles);
         listMatching.addAll(listMatchingBody);        
         return listMatching;
+    }
+    
+    private boolean searchByWebSite(Sites webSiteToSearch, String strSearch){
+        boolean matchingTitle = false;
+        webSiteToSearch.setListTokensMatches(null);
+        StringTokenizer tokens = new StringTokenizer(strSearch);
+        ArrayList<Token> listTokensMatches = new ArrayList<Token>();
+        int intMatch = 0;
+        Token currentToken = null;
+        while(tokens.hasMoreTokens()){                
+            int numberMatches = 0;
+            String token = tokens.nextToken();
+            // Compare the tokens with the title of the web-site
+            String textTitle = webSiteToSearch.getTitle().toLowerCase();
+            for(int n = 0; n< textTitle.length(); n++){
+                if(textTitle.charAt(n) == token.charAt(0)){
+                    int tempN = n; boolean match = true;
+                    for(int m = 0; m < token.length(); m++){
+                        if(textTitle.charAt(tempN) != token.charAt(m)){
+                            match = false;
+                            break;
+                        }
+                        tempN++;
+                    }
+                    if(match){
+                        numberMatches++;
+                    }
+                }
+            }
+            if(numberMatches > 0){ // If found any match in the tittle, this will be prioritized
+                matchingTitle = true;
+            }
+
+            // Compare the tokens with the body of the web-site
+            String textBody = webSiteToSearch.getBody().toLowerCase();
+            for(int n = 0; n < textBody.length(); n++){
+                if(textBody.charAt(n) == token.charAt(0)){
+                    int tempN = n; boolean match = true;
+                    for(int m = 0; m < token.length(); m++){
+                        if(textBody.charAt(tempN) != token.charAt(m)){
+                            match = false;
+                            break;
+                        }
+                        tempN++;
+                    }
+                    if(match){
+                        numberMatches++;
+                    }
+                }
+            }
+            // If exist more than zero matches of token, it added to list tokens match list
+            if(numberMatches > 0){
+                listTokensMatches.add(new Token(token, numberMatches));
+            }
+        }
+        if(!listTokensMatches.isEmpty()){
+            webSiteToSearch.setListTokensMatches(listTokensMatches);
+        }
+        return matchingTitle;
     }
     
     public void openWebSite(String webAdress){
